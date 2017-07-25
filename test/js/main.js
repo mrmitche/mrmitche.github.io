ad06eb501fe0f97b6eac52f9f33904344d94e628
mami <matt.mitchell@anevis-solutions.com>
2017-07-24 21:51:41 -0400

    added test page

---
+++ test/js/main.js
@@ -0,0 +1,24 @@
+function setHeight(){
+  var width = $('.project').width();
+  $('.project').height(0.28125*width+32); //based on 1280x720 image
+}
+
+function main(){
+
+  $('.tags p').on('click', function(){
+	var tagClass = '.' + $(this).text();
+	$('[class*="col-"]').hide(); //hides all project boxes
+	$(tagClass).show();
+	$('#reset').show(); //show reset button
+  });
+  
+  $('#reset').on('click', function(){
+	$('#reset').hide(); //hide reset button
+	$('[class*="col-"]').show(); //shows all
+  });
+  
+  setHeight();
+  window.onresize = setHeight;
+}
+
+$(document).ready(main);
