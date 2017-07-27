586d4aef26c52f2badaf4fae3fea6ddd3bb46df3
Himesh Patel <himeshapatel@gmail.com>
2017-07-27 10:31:34 -0400

    fixed up site, refer to readme for changes

---
+++ README.md
@@ -0,0 +1,4 @@
+# List of Changes
+*Used text instead of images for titles
+*height is no longer dependent on JS, CSS only (flexbox)
+*Tags don't scatter when resizing
--- test/js/main.js
+++ test/js/main.js
@@ -1,22 +1,22 @@
-function setHeight(){
-  var width = $('.project').width();
-  $('.project').height(0.28125*width+32); //based on 1280x720 image
-}
+// function setHeight() {
+//   var width = $('.project').width();
+//   $('.project').height(0.28125 * width + 32); //based on 1280x720 image
+// }
 
-function main(){
+function main() {
 
-  $('.tags p').on('click', function(){
-	var tagClass = '.' + $(this).text();
-	$('[class*="col-"]').hide(); //hides all project boxes
-	$(tagClass).show();
-	$('#reset').show(); //show reset button
+  $('.tags p').on('click', function() {
+    var tagClass = '.' + $(this).text();
+    $('[class*="col-"]').hide(); //hides all project boxes
+    $(tagClass).show();
+    $('#reset').show(); //show reset button
   });
-  
-  $('#reset').on('click', function(){
-	$('#reset').hide(); //hide reset button
-	$('[class*="col-"]').show(); //shows all
+
+  $('#reset').on('click', function() {
+    $('#reset').hide(); //hide reset button
+    $('[class*="col-"]').show(); //shows all
   });
-  
+
   setHeight();
   window.onresize = setHeight;
 }
